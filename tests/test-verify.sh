75db36ab407a9f56e6a845d0406a9b67a4c79b3e
Adam Dyson <contact@adamdyson.com.au>
2017-09-29 04:14:31 +1000

    Simplified testing role using Travis CI and docker containers.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,3 +2,8 @@
 
 # Verify the installed NFS server version.
 docker exec --tty ${container_id} env TERM=xterm /usr/sbin/rpcinfo -p | grep nfs
+
+if [ "${distribution}" = "ubuntu" ]; then
+  # Output all versions and priority for the package.
+  docker exec --tty ${container_id} env TERM=xterm apt-cache policy nfs-kernel-server
+fi
